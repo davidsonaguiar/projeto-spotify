b971db8abb3a386ed313ab26b43d74d074bcf1f7
davidsonaguiar <davidson.05@outlook.com>
2024-07-04 16:34:00 -0300

    configuração inicial

---
+++ src/main/java/com/poo/projetospotify/ProjetoSpotifyApplication.java
@@ -0,0 +1,13 @@
+package com.poo.projetospotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoSpotifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poo/projetospotify/ProjetoSpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poo.projetospotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
